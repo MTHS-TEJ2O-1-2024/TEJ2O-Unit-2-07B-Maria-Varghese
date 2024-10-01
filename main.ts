590e4efe581eb9be6c6d0f6f998292b15187d249
Maria-Varghese <maria.varghese@mths.ca>
2024-10-01 08:24:42 -0700

    showing cookie numbers

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Maria
+ * Created on: Sep 2024
+ * This program shows the cookie numbers
 */
 
-basic.showString('Hello, World!')
+// our cookie numbers
+let cookieNumber: number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function(){
+    // get cookieNumber
+    cookieNumber= cookieNumber + 1
+
+    //output answer
+    basic.showNumber(0 + 1)
+
+})
+
+input.onButtonPressed(Button.B, function () {
+    // get cookieNumber
+    cookieNumber = 0
+
+    //output answer
+    basic.showNumber(0)
+})
